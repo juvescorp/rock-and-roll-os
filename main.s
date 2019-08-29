7c5d5e603f1a3d1d0c7a8a564900e0dbff9aa4cb
juvescorp <46848266+juvescorp@users.noreply.github.com>
2019-08-29 15:27:17 +0300

    change // for # (заменить // на #)
    For comments (для обозначения комментариев)

--- main.s
+++ main.s
@@ -1,16 +1,16 @@
-.code16 // tells GAS to output 16-bit code / Компиляция в 16-битный код
-    mov $msg, %si // put msg address into si / Положить адрес сообшщения msg в si
-    mov $0x0e, %ah // put 0x0e into ah (function of int 10h - print a string) 
-    // Положить в ah 0x0e (номер функции int 10h - вывести строку на экран)
+.code16 # tells GAS to output 16-bit code / Компиляция в 16-битный код
+    mov $msg, %si # put msg address into si / Положить адрес сообшщения msg в si
+    mov $0x0e, %ah # put 0x0e into ah (function of int 10h - print a string) 
+    # Положить в ah 0x0e (номер функции int 10h - вывести строку на экран)
 loop:
-    lodsb // move byte from address ds:si to al and add 1 to si 
-    // Считать байт по адресу DS:(E)SI в AL и добавить 1 к SI
-    or %al, %al // logical or // логическое или
-    jz halt // if zf(zero flag)=0 (means that al=0) then halt the system 
-    // если zf(флаг нуля)=0 (это значит, что al=0), то останавливаем систему
-    int $0x10 // print symbol on a screen // вывести символ на экран
-    jmp loop // jump to "loop", next step of a cycle // перейти к метке loop, на следующий шаг цикла
+    lodsb # move byte from address ds:si to al and add 1 to si 
+    # Считать байт по адресу DS:(E)SI в AL и добавить 1 к SI
+    or %al, %al # logical or // логическое или
+    jz halt # if zf(zero flag)=0 (means that al=0) then halt the system 
+    # если zf(флаг нуля)=0 (это значит, что al=0), то останавливаем систему
+    int $0x10 # print symbol on a screen // вывести символ на экран
+    jmp loop # jump to "loop", next step of a cycle // перейти к метке loop, на следующий шаг цикла
 halt: 
-    hlt // halt the system // остановить систему
+    hlt # halt the system // остановить систему
 msg:
-    .asciz "hello world" // string for printing, ending with zero // строка для вывода, заканчивается нулём
+    .asciz "hello world" # string for printing, ending with zero // строка для вывода, заканчивается нулём
