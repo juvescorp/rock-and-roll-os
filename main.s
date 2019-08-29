452f462654aed646d55ef412a2526171055b65d5
juvescorp <46848266+juvescorp@users.noreply.github.com>
2019-08-29 14:51:54 +0300

    Update main.s

--- main.s
+++ main.s
@@ -1,13 +1,14 @@
-.code16
-    mov $msg, %si
-    mov $0x0e, %ah
+.code16 // tells GAS to output 16-bit code / Компиляция в 16-битный код
+    mov $msg, %si // put msg address into si / Положить адрес сообшщения msg в si
+    mov $0x0e, %ah // put 0x0e into ah (function of int 10h - print a string) 
+    // Положить в ah 0x0e (номер функции int 10h - вывести строку на экран)
 loop:
-    lodsb
-    or %al, %al
-    jz halt
-    int $0x10
-    jmp loop
-halt:
-    hlt
+    lodsb // move byte from address ds:si to al // Считать байт по адресу DS:(E)SI в AL
+    or %al, %al // logical or // логическое или
+    jz halt // if al=0 then halt the system // если al=0, то останавливаем систему
+    int $0x10 // print symbol on a screen // вывести символ на экран
+    jmp loop // jump to "loop", next step of a cycle // перейти к метке loop, на следующий шаг цикла
+halt: 
+    hlt // halt the system // остановить систему
 msg:
-    .asciz "hello world"
+    .asciz "hello world" // string for printing, ending with zero // строка для вывода, заканчивается нулём
